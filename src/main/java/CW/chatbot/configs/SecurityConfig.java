8ce166392a7a0985bbffba59a6a736a10f94fe4a
hjuuK <khj020508@naver.com>
2024-03-29 01:24:29 +0900

    Revert "Spring Security 설정"
    This reverts commit 5aece41e5bfc7c540b5a922e5da581939be4e344.

--- src/main/java/CW/chatbot/configs/SecurityConfig.java
+++ src/main/java/CW/chatbot/configs/SecurityConfig.java
@@ -5,7 +5,6 @@ import CW.chatbot.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,27 +18,28 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
-                .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                // JWT 사용하기 때문에 세션을 사용하지 않음
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        // 해당 API에 대해서는 모든 요청을 허가
-                        .requestMatchers("/members/login_select").permitAll()
-                        // USER 권한이 있어야 요청할 수 있음
-                        .requestMatchers("/members/test").hasRole("USER")
-                        // 이 밖에 모든 요청에 대해서 인증 필요
-                        .anyRequest().authenticated())
-                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
-        return httpSecurity.build();
-    }
+    // 코드 수정 예정
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                // JWT 사용하기 때문에 세션을 사용하지 않음
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeHttpRequests()
+//                // 해당 API에 대해서는 모든 요청을 허가
+//                .requestMatchers("/members/login_select").permitAll()
+//                // USER 권한이 있어야 요청할 수 있음
+//                .requestMatchers("/members/test").hasRole("USER")
+//                // 이 밖에 모든 요청에 대해서 인증 필요
+//                .anyRequest().authenticated()
+//                .and()
+//                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+//                        UsernamePasswordAuthenticationFilter.class).build();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
