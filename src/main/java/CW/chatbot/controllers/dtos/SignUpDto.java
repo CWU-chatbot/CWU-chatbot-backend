cffbfbedd1fffdd814c4e310f630e61f623e3145
hjuuK <khj020508@naver.com>
2024-04-09 11:44:42 +0900

    회원가입 Service 구현

---
+++ src/main/java/CW/chatbot/controllers/dtos/SignUpDto.java
@@ -0,0 +1,33 @@
+package CW.chatbot.controllers.dtos;
+
+import CW.chatbot.commons.constants.Role;
+import CW.chatbot.entities.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpDto {
+    private String id;
+    private String password;
+    private String username;
+    private Set<Role> roles;
+    private LocalDateTime registrationDate;
+
+    public Member toEntity(String encodedPassword, Set<Role> roles) {
+        return Member.builder()
+                .id(id)
+                .password(encodedPassword)
+                .username(username)
+                .roles(roles)
+                .registrationDate(registrationDate)
+                .build();
+    }
+}
