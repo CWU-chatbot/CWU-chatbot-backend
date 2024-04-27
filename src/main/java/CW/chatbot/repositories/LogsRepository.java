e7095b8a59d8ccc20b20436c5c5d754ae1af67bb
zibago <cownsgur01@gmail.com>
2024-04-28 03:12:39 +0900

    repository 수정

--- src/main/java/CW/chatbot/repositories/LogsRepository.java
+++ src/main/java/CW/chatbot/repositories/LogsRepository.java
@@ -1,9 +1,9 @@
 package CW.chatbot.repositories;
 
-import CW.chatbot.entities.ChatbotResponse;
+import CW.chatbot.entities.LOGS;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatbotResponseRepository extends JpaRepository<ChatbotResponse, Long> {
+public interface LogsRepository extends JpaRepository<LOGS, Long> {
 }
