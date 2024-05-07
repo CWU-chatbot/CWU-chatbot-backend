9160cb41b52620cbe0400182c15f986c041aa860
zibago <cownsgur01@gmail.com>
2024-05-07 10:56:56 +0900

    주석 추가

--- src/main/java/CW/chatbot/services/ChatService.java
+++ src/main/java/CW/chatbot/services/ChatService.java
@@ -14,6 +14,34 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 챗봇 응답과 관련된 서비스 로직을 제공하는 서비스 클래스.
+ * 이 클래스는 사용자 질문에 대한 챗봇의 응답을 처리하고, 해당 대화 로그를 데이터베이스에 저장하는 기능을 수행합니다.
+ * <p>
+ * 주요 기능:
+ * - 챗봇 서비스와의 통신을 관리하여 사용자 질문에 대한 답변을 가져옵니다.
+ * - 사용자의 질문과 챗봇의 응답을 로그로 저장합니다.
+ * <p>
+ * 구성 요소:
+ * - RestTemplate: HTTP 요청을 보내기 위한 스프링의 헬퍼 클래스.
+ * - LogsRepository: 대화 로그를 데이터베이스에 저장하기 위한 JPA 리포지토리.
+ * - JwtTokenProvider: JWT 토큰에서 사용자 정보를 추출하는 유틸리티 클래스.
+ * - chatbotServiceUrl: 외부 챗봇 서비스의 URL, application.properties 파일에서 설정된 값을 주입받습니다.
+ * <p>
+ * 메소드 설명:
+ * - getChatbotResponse(int folderId, String userQuestion, String token): 사용자의 질문을 받아 챗봇 서비스에 요청하고,
+ * 챗봇의 응답을 반환합니다. 사용자 질문과 챗봇의 응답은 LOGS 테이블에 저장됩니다. 메소드는 사용자의 토큰을 검증하고,
+ * 토큰에서 사용자 ID를 추출하여 사용합니다. 문제가 발생한 경우 ChatExceptionDTO 예외를 발생시킵니다.
+ * <p>
+ * 예외 처리:
+ * - ChatExceptionDTO: 챗봇 서비스 통신 중 발생한 예외를 적절히 처리하고, 상태 코드와 에러 메시지를 포함하는
+ * 사용자 정의 예외를 발생시킵니다.
+ * <p>
+ * 사용 예:
+ * - 이 서비스는 챗봇과의 인터랙션을 관리하는 컨트롤러에서 호출됩니다. 컨트롤러는 사용자의 HTTP 요청을 받아 이 서비스의
+ * 메소드를 사용하여 챗봇 응답을 처리하고 최종 결과를 사용자에게 반환합니다.
+ */
+
 @Service
 public class ChatService {
 
--- src/main/java/CW/chatbot/services/MemberService.java
+++ src/main/java/CW/chatbot/services/MemberService.java
@@ -16,6 +16,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 사용자 인증 및 회원 가입 관련 서비스를 제공하는 클래스.
+ * 이 클래스는 사용자 로그인 및 회원가입 로직을 처리하고, JWT 토큰을 발급하여 사용자 세션을 관리합니다.
+ * <p>
+ * 주요 기능:
+ * - 로그인: 사용자의 ID와 비밀번호를 검증하고 JWT 토큰을 발급합니다.
+ * - 회원가입: 새 사용자를 등록하고 기본적인 권한을 설정합니다.
+ * <p>
+ * 구성 요소:
+ * - UsersRepository: 사용자 데이터에 대한 데이터베이스 접근을 처리합니다.
+ * - AuthenticationManagerBuilder: Spring Security의 인증 메커니즘을 구성합니다.
+ * - JwtTokenProvider: 인증 성공 후 사용자 정보를 바탕으로 JWT 토큰을 생성합니다.
+ * - PasswordEncoder: 비밀번호 암호화를 담당합니다.
+ * <p>
+ * 메소드 설명:
+ * - login(String id, String password): 사용자가 제공한 ID와 비밀번호로 인증을 시도하고, 성공 시 JWT 토큰을 반환합니다.
+ * - signUp(SignUpReqDto signUpReqDto): 회원가입 요청 데이터를 바탕으로 새로운 사용자를 생성하고 저장합니다.
+ * <p>
+ * 예외 처리:
+ * - 로그인 과정에서 ID 또는 비밀번호가 일치하지 않는 경우, AuthenticationException을 통해 처리됩니다.
+ * - 회원가입 과정에서 ID가 중복되는 경우, IllegalArgumentException을 발생시켜 사용자에게 알립니다.
+ * <p>
+ * 사용 예:
+ * - 이 서비스는 컨트롤러에서 사용자의 로그인 요청을 처리하거나 회원가입 요청을 받을 때 호출됩니다.
+ * 사용자 인증이나 회원 데이터의 생성 및 검증을 담당합니다.
+ *
+ * @Transactional 어노테이션:
+ * - 클래스 레벨에서 readOnly = true 옵션으로 설정되어 있어, 대부분의 메소드가 읽기 전용 트랜잭션에서 실행됩니다.
+ * - 변경 작업이 필요한 메소드(login 및 signUp)에는 별도의 @Transactional 어노테이션이 적용되어 있어, 해당 작업 중 오류 발생 시 전체 작업을 롤백합니다.
+ */
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -58,7 +89,7 @@ public class MemberService { // 서비스 클래스 - 로그인 메서드 구현
         usersRepository.save(USERS);
 
         log.info("회원가입 성공 ID: {}", signUpReqDto.getUserid());
-        
+
         return MemberSignupDto.toDto(USERS);
     }
 }
